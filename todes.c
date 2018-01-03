93d013dc3b4d3e8560fa4e0f712497ebf2c1e570
Yunpeng.Ma <791374534@qq.com>
2018-01-03 19:46:18 +0800

    add desto.c, todes.c, echo_stdclnt.c and echo_stdserv.c

---
+++ todes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(void)
+{
+	FILE *fp;
+	int fd=open("data.dat", O_WRONLY|O_CREAT|O_TRUNC);
+	if(fd==-1)
+	{
+		fputs("file open error", stdout);
+		return -1;
+	}
+
+	printf("First file description: %d \n", fd);
+	fp=fdopen(fd, "w");
+	fputs("TCP/IP SOCKET PROGRAMMING \n", fp);
+	printf("Second file description: %d \n", fileno(fp));
+	fclose(fp);
+	return 0;
+}
+
